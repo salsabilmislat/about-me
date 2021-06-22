5b9256a58cc2a255ee582d373ae0afbe6483fc20
salsabilmislat <salsabilmislat196@gmail.com>
2021-06-22 17:37:15 +0300

    new js

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # about-me
+
+## This website will be  an Guessing Game & some information's about me as an owner of this site
+
+***Hope you enjoy***
--- js/app.js
+++ js/app.js
@@ -4,12 +4,13 @@ let username = prompt('Type your name please');
 //console.log(username);
 alert('welcome to our site ' + username + '  My name is salsabil nice to meet you');
 
-
+let score = 0;
 
 let age = Number(prompt('Can you guess my age ?'));
 if (age == 25) {
-    alert('your are corect, I am 25 years old ');
-    //console.log('your are corect, I am 25 years old ');
+    alert('your are correct, I am 25 years old ');
+    //console.log('your are correct, I am 25 years old ');
+    score++;
 }
 else {
     alert('That is not right ')
@@ -17,31 +18,33 @@ else {
 
 
 
-let language = prompt('Do I speak more than one language ?');
+let language = prompt('Do I speak more than one language ?', 'answer with yes or no plz');
 
 switch (language.toLowerCase()) {
     case 'yes':
     case 'y':
         alert('Good job😉 that is right i could speak three languages');
         //console.log('Good job😉 that is right i could speak three languages');
+        score++;
         break;
     case 'no':
     case 'n':
-        alert('That is oaky, you can git it next time. i do speak more than one');
-        //console.log('That is oaky, you can git it next time. i do speak more than one');
+        alert('That is okay, you can git it next time. i do speak more than one');
+        //console.log('That is okay, you can git it next time. i do speak more than one');
         break;
 }
 
 
 
 
-let china = prompt('Do I Think that chinese language is the most hard lang. In the world ?');
+let china = prompt('Do I Think that chinese language is the most hard lang. In the world ?', 'answer with yes or no plz');
 //console.log(china);
-function programing() {
-    if (china == "yes") {
-        alert('your are corect, it\'s hard to learn it. But in time you will love it ');
+function chinaQ() {
+    if (china.toLocaleLowerCase() == "yes" || china.toLocaleLowerCase() === 'y') {
+        alert('your are correct, it\'s hard to learn it. But in time you will love it ');
+        score++;
     }
-    else if (china == "no") {
+    else if (china.toLocaleLowerCase == "no" || china.toLocaleLowerCase() === 'n') {
         alert(' unfortunately, I Do. Because it will take too much time and effort to learn it ');
     }
     else {
@@ -49,16 +52,17 @@ function programing() {
     }
 }
 
-programing();
+chinaQ();
 
 
-let visit = prompt('Do you think that i visited china ?');
+let visit = prompt('Do you think that i visited china ?', 'answer with yes or no plz');
 
 switch (visit.toLowerCase()) {
     case 'yes':
     case 'y':
         alert('That is right, i have been there for one year. and i really enjoy it.');
         //console.log('That is right i have been there for one year. and i really enjoy it.');
+        score++;
         break;
     case 'no':
     case 'n':
@@ -68,17 +72,64 @@ switch (visit.toLowerCase()) {
 
 }
 
-        let capitalcity = prompt("Do you know the name of china capital city?")
-        switch (capitalcity.toLowerCase()) {
-            case 'yes':
-            case 'y':
-                alert(username + '  Good job😉 beijing is china\'s capital city ')
-                //console.log('Good job😉 beijing is china\'s capital city');
-                break;
-            case 'no':
-            case 'n':
-                alert(username + ' Here is the answer.... beijing is china\'s capital city ')
-                //console.log('Here is the answer.... beijing is china\'s capital city');
-                break;
+let City = prompt("Do you know the name of china capital city?", 'answer with yes or no plz');
+switch (City.toLowerCase()) {
+    case 'yes':
+    case 'y':
+        alert(username + '  Good job😉 beijing is china\'s capital city ');
+        //console.log('Good job😉 beijing is china\'s capital city');
+        score++;
+        break;
+    case 'no':
+    case 'n':
+        alert(username + ' Here is the answer.... beijing is china\'s capital city ');
+        //console.log('Here is the answer.... beijing is china\'s capital city');
+        break;
+
+}
+let guess = 1;
+while (guess <= 4) {
+    let guessNumber = Number(prompt('let\'s play Guess a number game?' + ' Be carful you only got 4 chances', 'The answer would be between 1-20'));
+    if (guessNumber == 12) {
+        alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN " + guess + " GUESS ");
+        score++;
+        break;
+    } else if (guessNumber > 12) {
+        guess++;
+        alert("The number that you enter is too hight");
+
+    }
+    else {
+        guess++;
+        alert("The number that you enter is too low");
+    }
+
+}
+alert(" The correct answer was 12");
+//console.log(guessNumber);
+let F = ['kabsa', 'maqloba', 'fried chicken', 'pizza', "burger"];
+let chances = 1;
+while (chances <= 6) {
+    let Food = prompt('let\'s play Guess my favorites food  game?', ' Be carful you only got 6 chances');
+    for (let i = 0; i < F.length; i++) {
+        if (Food == F[i]) {
+
+            // alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT It was one of This list Kabsa, Maqloba, Fried chicken and pizza ");
+            score++;
+            chances = 7;
+            break;
+        }
+        else {
+
 
         }
+    }
+    chances++;
+    // alert("That is not on of my favorites food list! try again");
+
+}
+alert("The correct answer was one of my favorites food list Kabsa, Maqloba, Fried chicken and pizza");
+//console.log("end");
+
+alert('Great Job ' + username + ' and your score is ' + score);
+
